7007a34ca098bc97086a243b0e2278b945eba73a
pavel <p-vikulinpb@mail.ru>
2025-09-19 01:26:54 +0300

    Переделано все на jwt токены

--- src/main/java/org/example/aideepseek/controller/SubscriptionInfoController.java
+++ src/main/java/org/example/aideepseek/controller/SubscriptionInfoController.java
@@ -1,9 +1,13 @@
 package org.example.aideepseek.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.example.aideepseek.database.service.GetSubscriptionByEmail;
+import org.example.aideepseek.security.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -11,9 +15,22 @@ public class SubscriptionInfoController {
 
     @Autowired
     private GetSubscriptionByEmail getSubscriptionByEmail;
+    @Autowired
+    private JwtUtil jwtUtil;
 
-    @GetMapping("/subscription/online/{username}")
-    public ResponseEntity<?> getSubscriptionUser(@PathVariable String username){
+    @GetMapping("/subscription/online")
+    public ResponseEntity<?> getSubscriptionUser(){
+
+        String username = null;
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            username = jwtUtil.extractUsername(authHeader.substring(7));
+        }
+
         return ResponseEntity.ok().body(getSubscriptionByEmail.getSubscriptionByEmail(username));
     }
 }
--- src/main/java/org/example/aideepseek/controller/SubscriptionTransactionController.java
+++ src/main/java/org/example/aideepseek/controller/SubscriptionTransactionController.java
@@ -1,22 +1,38 @@
 package org.example.aideepseek.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.example.aideepseek.database.service.GetTransactionByEmail;
+import org.example.aideepseek.security.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @RestController
 @RequestMapping("/api/v1")
 public class SubscriptionTransactionController {
 
     @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
     private GetTransactionByEmail getTransactionByEmail;
 
-    @GetMapping("/subscription/online/transaction/{username}")
-    public ResponseEntity<?> getSubscriptionTransactionUser(@PathVariable String username){
+    @GetMapping("/subscription/online/transaction")
+    public ResponseEntity<?> getSubscriptionTransactionUser(){
+        String username = null;
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            username = jwtUtil.extractUsername(authHeader.substring(7));
+        }
+
         return ResponseEntity.ok().body(getTransactionByEmail.getTransaction(username));
     }
 }
